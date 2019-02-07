7c92dfb654bd7638ca199baf3eae1c7add465a76
cakrit <chris@netdata.cloud>
2019-02-07 10:36:57 +0100

    buildhtml & netlify.toml

---
+++ buildhtml.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Fetches the netdata repo and builds the documentation HTML site
+# With this script, we can ensure that any changes to the markdown files of this repo
+# that are included in the main docs will not break the HTML generation.
+# The script is called by netlify whenever something changes to the repo
+
+set -e
+
+echo "Cloning the netdata repo"
+git clone https://github.com/netdata/netdata.git netdata
+
+cd netdata
+
+echo "Calling the netdata docs/generator/buildhtml.sh"
+./docs/generator/buildhtml.sh
+
+echo "HTML Documentation produced under netdata/generator/build"
+
+
+
+
